179128bcd1baf5f8816f23dacd2a5489da787e8e
Thomas Castiglione <castiglione@gmail.com>
2013-07-02 22:55:47 +0800

    use Dispatch to connect to the site

--- build.sbt
+++ build.sbt
@@ -9,7 +9,9 @@ scalaVersion := "2.10.2"
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 libraryDependencies ++= Seq (
-	"org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
+  "org.scalatest" %% "scalatest" % "1.9.1" % "test", 
+  "net.databinder.dispatch" %% "dispatch-core" % "0.10.1",
+  "net.databinder.dispatch" %% "dispatch-tagsoup" % "0.10.1"
 )
 
 seq(SbtStartScript.startScriptForClassesSettings: _*)
---
+++ src/main/scala/london/PipelinedClient.scala
@@ -0,0 +1,42 @@
+package london
+import scala.collection.mutable
+import scala.collection.JavaConversions._
+import dispatch._, Defaults._
+import com.ning.http.client._
+
+class PipelinedClient {
+  val client = dispatch.Http()
+  val cookies = mutable.Set[Cookie]()
+  
+  //synchronous dispatch extensions - sadly, clay men have no Future[T]  
+  def command(builder: RequestBuilder) {
+    val request = client(builder)
+    val response = request()
+    for (cookie <- response.getCookies())
+      cookies += cookie
+  } 
+  
+  def query(builder: RequestBuilder) = {
+    for(cookie <- cookies)
+      builder.addCookie(cookie)
+    val request = client(builder > as.tagsoup.NodeSeq)
+    request()
+  }
+  
+  //extra bits for handling redirects explicitly - not currently used
+  implicit class RequestHandlerExtensions(builder: RequestBuilder) {
+    def FOUND [T](f: Response => T) =
+      ((builder.build(), new FoundFunctionHandler(f)))
+  }
+
+  class FoundFunctionHandler[T](f: Response => T) extends FunctionHandler[T](f) with FoundHandler[T]
+
+  trait FoundHandler[T] extends AsyncHandler[T] {
+    abstract override def onStatusReceived(status: HttpResponseStatus) = {
+      if (status.getStatusCode / 302 == 2)
+        super.onStatusReceived(status)
+      else
+        throw StatusCode(status.getStatusCode)
+    }
+  }
+}
