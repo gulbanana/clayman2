702a96540de3e22f772f9883b6108ecb4d232bb0
Thomas Castiglione <castiglione@gmail.com>
2013-07-11 22:23:55 +0800

    use did(){}

--- src/main/scala/jobs/courtGrind.scala
+++ src/main/scala/jobs/courtGrind.scala
@@ -4,21 +4,19 @@ import common._
 
 //hang around the court grinding
 object courtGrind extends OneManJob {
-  def apply(implicit c: Character) = repeat {
-    if (c.location != Areas.Court)
-      getToCourt(c)
-      
-    else
-      atCourt(c)
-  }
-  
-  private def getToCourt(implicit c: Character) {
+  def apply(implicit c: Character): Unit = repeat {
     gear.persuasive()
     
-    if (c.qualities("Connected: the Duchess") < 10)
+    if (getToCourt) return
+    if (reduceMenaces) return
+    if (gainFascinating) return
+    grind
+  }
+  
+  private def getToCourt(implicit c: Character) = did (c.location != Areas.Court) {  
+    if (c.qualities("Connected: the Duchess") < 10) {
       connected.the_duchess()
-      
-    else {
+    } else {
       c.travel(Areas.ShutteredPalace)
       c.beginStorylet("Spend a few days at Court")
       c.chooseBranch("A word from the Duchess")
@@ -26,31 +24,25 @@ object courtGrind extends OneManJob {
     }
   }
   
-  private def atCourt(implicit c: Character) {
-    gear.persuasive()
-    
-    //use up Fascinating, if available
-    if (c.qualities("Scandal") > 0 && c.qualities("Fascinating...") >= 4)
-      court.fix_scandal()
-    else if (c.qualities("Wounds") > 0 && c.qualities("Fascinating...") >= 5)
-      court.fix_wounds()
-      
-    //prioritise grinds that don't cause Scandal and gain Fascinating up to a useful level
-    else if (c.items("Whispered Secret") < 80000) //buy the lofty tower
-      court.grind_mysteries()
-    else if (c.qualities("Fascinating...") < 5)
-      court.grind_wines()
-
-    //if we've already got Fascinating:
-    else if (c.items("Prisoner's Honey") < 40000) //buy the royal bethlehem
-      court.grind_honey()
-    else if (c.items("Proscribed Material") < 15000)
-      court.grind_rumour() 						  //buy the western tower (by converting to souls)
-    else if(c.qualities("Scandal") < 7)
-      court.grind_influence()
-    
-    //safe money fallback - should never be reached, though
-    else
-      court.grind_jade()
+  private def reduceMenaces(implicit c: Character) = did (c.qualities("Scandal") > 0 && c.qualities("Fascinating...") >= 4) {    
+    court.fix_scandal()
+  } or (c.qualities("Wounds") > 0 && c.qualities("Fascinating...") >= 5) {
+    court.fix_wounds()
+  }
+  
+  private def gainFascinating(implicit c: Character) = did (c.items("Whispered Secret") < 80000) { 
+    court.grind_mysteries() //buy the lofty tower
+  } or (c.qualities("Fascinating...") < 5) {
+    court.grind_wines()
+  }
+  
+  private def grind(implicit c: Character) = did (c.items("Prisoner's Honey") < 40000) { 
+    court.grind_honey()     //buy the royal bethlehem
+  } or (c.items("Proscribed Material") < 15000) {
+    court.grind_rumour() 	  //buy the western tower (by converting to souls)
+  } or (c.qualities("Scandal") < 7) {
+    court.grind_influence() 
+  } or {
+    court.grind_jade()      //safe money fallback - should never be reached, though
   }
 }
