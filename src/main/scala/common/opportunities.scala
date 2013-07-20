335c78ecbdd9d45ca5fc8836ebd950b13caba69a
Thomas Castiglione <castiglione@gmail.com>
2013-07-20 13:45:19 +0800

    new bad cards

--- src/main/scala/common/opportunities.scala
+++ src/main/scala/common/opportunities.scala
@@ -17,7 +17,8 @@ object opportunities {
     "Stark villainy",                                       //36 rostygold
     "The marriage of inconvenience",                        //50 whispered secrets
     "Rob a Brass Embassy courier",                          //15 proscribed material
-    "Academic discipline",                                  //60 '79
+    "Academic discipline",                                  //60 '79,
+    "A chance encounter with an old friend...",             //quirk gains cap at 6
     "The Ways of the University",
     "The Ways of the Forgotten Quarter",
     "The Ways of Wolfstack Docks",
