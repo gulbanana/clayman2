9718b6b1fba00b48ebb564f30e38ee9e5f8231fa
Thomas Castiglione <castiglione@gmail.com>
2013-07-28 13:51:38 +0800

    comment was wronggit add -A

--- src/main/scala/jobs/flitThefts.scala
+++ src/main/scala/jobs/flitThefts.scala
@@ -3,7 +3,6 @@ import london._
 import common._
 import david._
 
-//just a test script
 object flitThefts extends OneManJob {
   def apply(implicit c: Character) = did (c.qualities("Suspicion") < 7 && c.qualities("Casing...") < 20) {
     heist.casing4()
