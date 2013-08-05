b59cf1db6f7fa728ca424e46b189d1ea88c22675
gulbanana <castiglione@mac.com>
2013-08-05 14:28:25 +0800

    Update gear.scala

--- src/main/scala/david/gear.scala
+++ src/main/scala/david/gear.scala
@@ -17,7 +17,7 @@ object gear {
     c.equip("Cracksman's Mittens")
     c.equip("Kifers")
     c.equip("Hushed Spidersilk Slippers")
-    c.equip("Devious Henchman")
+    c.equip("Unscrupulous Raven Advisor")
   }
   
   def dangerous()(implicit c: Character) {
