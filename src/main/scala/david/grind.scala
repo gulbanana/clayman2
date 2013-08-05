30526d2da1371eb2e3db975ede72799445dc8c9a
Thomas Castiglione <castiglione@gmail.com>
2013-08-06 02:33:10 +0800

    unfinished business

--- src/main/scala/david/grind.scala
+++ src/main/scala/david/grind.scala
@@ -5,25 +5,122 @@ object grind {
   /*********/
   /* GOODS */
   /*********/
-  //+54 rostygold with a rare success for appalling secrets
-  def rostygold()(implicit c: Character) {
+  //+100 amber
+  def deep_amber()(implicit c: Character) {
+    gear.persuasive()
+    c.travel(Areas.Veilgarden)
+    c.beginStorylet("Unfinished Business in Veilgarden")
+    c.chooseBranch("Get in on a card game")
+    c.onwards()
+  }
+  
+  //+5 contracts
+  def infernal_contracts()(implicit c: Character) {
+    gear.dangerous()
+    c.travel(Areas.WatchmakersHill)
+    c.beginStorylet("Unfinished Business in Watchmaker's Hill")
+    c.chooseBranch("Contracts and legs")
+    c.onwards()
+  }
+  
+  //+100 pearls 
+  def moon_pearls()(implicit c: Character) {
+    gear.dangerous()
+    c.travel(Areas.WatchmakersHill)
+    c.beginStorylet("Unfinished Business in Watchmaker's Hill")
+    c.chooseBranch("A spot of footpadry")
+    c.onwards()
+  }
+  
+  //+550 brass for 5 actions 
+  def nevercold_brass_slivers()(implicit c: Character) {
+    gear.watchful()
     c.travel(Areas.LadybonesRoad)
-    c.beginStorylet("Industrial Espionage!")
-    c.chooseBranch("Investigate the Embassy warehouses")
+    c.beginStorylet("Unfinished Business in Ladybones Road")
+    c.chooseBranch("Mischief and brass")
+    c.onwards()
+  }
+  
+  //+100 rats
+  def rats_on_strings()(implicit c: Character) {
+    gear.shadowy()
+    c.travel(Areas.Spite)
+    c.beginStorylet("Unfinished Business in Spite")
+    c.chooseBranch("Keep your hands quick")
+    c.onwards()
+  }
+  
+  //+40 relics for 2 actions
+  def relics_of_the_fourth_city()(implicit c: Character) {
+    gear.watchful()
+    c.travel(Areas.LadybonesRoad)
+    c.beginStorylet("Unfinished Business in Ladybones Road")
+    c.chooseBranch("The study of antiquities")
+    c.onwards()
+  }
+  
+  //+550 rostygold for 5 actions 
+  def rostygold()(implicit c: Character) {
+    gear.dangerous()
+    c.travel(Areas.WatchmakersHill)
+    c.beginStorylet("Unfinished Business in Watchmaker's Hill")
+    c.chooseBranch("Wade into the Ring Fights")
+    c.onwards()
+  }
+  
+  //+250 currency for 6 actions (1.25E - higher difficulty..)
+  def surface_currency()(implicit c: Character) {
+    gear.persuasive()
+    c.travel(Areas.Veilgarden)
+    c.beginStorylet("Unfinished Business in Veilgarden")
+    c.chooseBranch("Shake down your agent")
+    c.onwards()
+  }
+  
+  /************/
+  /* ACADEMIC */
+  /************/
+  //+100 candles
+  def foxfire_candle_stubs()(implicit c: Character) {
+    gear.persuasive()
+    c.travel(Areas.Veilgarden)
+    c.beginStorylet("Unfinished Business in Veilgarden")
+    c.chooseBranch("An admirer among the clergy")
+    c.onwards()
+  }
+  
+  /***************/
+  /* CARTOGRAPHY */
+  /***************/
+  //+100 glim
+  def glim()(implicit c: Character) {
+    gear.shadowy()
+    c.travel(Areas.Spite)
+    c.beginStorylet("Unfinished Business in Spite")
+    c.chooseBranch("Rob a glim shipment")
     c.onwards()
   }
 
   /*********/
   /* ELDER */
   /*********/
-  //+82 jade fragments (0.82E)
-  //failure: 2cp scandal
+  //+240 jade for 2 actions (1.2E) 
   def jade_fragments()(implicit c: Character) {
-    c.travel(Areas.ForgottenQuarter)
-    c.beginStorylet("A tour of the quarter")
-    c.chooseBranch("The exploitation of knowledge")
+    gear.dangerous()
+    c.travel(Areas.WatchmakersHill)
+    c.beginStorylet("Unfinished Business in Watchmaker's Hill")
+    c.chooseBranch("A sure bet")
     c.onwards()
-  } 
+  }
+  
+  //+11 relics, -10 rostygold
+  def relics_of_the_third_city()(implicit c: Character) {
+    gear.shadowy()
+    c.travel(Areas.Spite)
+    c.beginStorylet("Unfinished Business in Spite")
+    c.chooseBranch("The fellow in the corner")
+    c.onwards()
+  }
   
   //12+ actions for 1 mystery (12.5E)
   def antique_mysteries()(implicit c: Character) {
@@ -37,11 +134,12 @@ object grind {
   /************/
   /* INFERNAL */
   /************/
-  //+28 soul (0.56E)
+  //+50 souls
   def souls()(implicit c: Character) {
-    c.travel(Areas.LadybonesRoad)
-    c.beginStorylet("Track down a Spirifer")
-    c.chooseBranch("Watch from the rooftops")
+    gear.shadowy()
+    c.travel(Areas.Spite)
+    c.beginStorylet("Unfinished Business in Spite")
+    c.chooseBranch("The prince of...")
     c.onwards()
   }
   
@@ -64,8 +162,50 @@ object grind {
   }
   
   /*************/
+  /* INFLUENCE */
+  /*************/
+  //+40 correspondence
+  def stolen_correspondence()(implicit c: Character) {
+    gear.shadowy()
+    c.travel(Areas.Spite)
+    c.beginStorylet("Unfinished Business in Spite")
+    c.chooseBranch("Ambush a few couriers for old times' sake")
+    c.onwards()
+  }
+  
+  //12+ actions for 1 (12.5E)
+  def bazaar_permits()(implicit c: Character) {
+    gear.shadowy()
+    if (c.qualities("Casing...") < 10)
+      flit.casing_optimal()
+    else
+      flit.steal_bazaar_permit()
+  }
+  
+  /**************/
+  /* LUMINOSITY */
+  /**************/
+  //+100 beeswax
+  def lamplighter_beeswax()(implicit c: Character) {
+    gear.watchful()
+    c.travel(Areas.LadybonesRoad)
+    c.beginStorylet("Unfinished Business in Ladybones Road")
+    c.chooseBranch("A lovely thought")
+    c.onwards()
+  }
+  
+  /*************/
   /* MYSTERIES */
   /*************/
+  //+50 clues
+  def cryptic_clues()(implicit c: Character) {
+    gear.shadowy()
+    c.travel(Areas.Spite)
+    c.beginStorylet("Unfinished Business in Spite")
+    c.chooseBranch("Eavesdropping")
+    c.onwards()
+  }
+  
   //12+ actions for 25 tales (12.5E)
   def tales_of_terror()(implicit c: Character) {
     gear.shadowy()
@@ -85,14 +225,71 @@ object grind {
   }
   
   /*************/
-  /* MYSTERIES */
+  /* NOSTALGIA */
   /*************/
-  //12+ actions for 1 (12.5E)
-  def bazaar_permits()(implicit c: Character) {
+  //+50 honey (rare: +1 stolen kiss)
+  def prisoners_honey()(implicit c: Character) {
+    gear.persuasive()
+    c.travel(Areas.Veilgarden)
+    c.beginStorylet("Unfinished Business in Veilgarden")
+    c.chooseBranch("Doing the decent thing.")
+    c.onwards()
+  }
+
+  /*************/
+  /* RAG TRADE */
+  /*************/
+  //+450 silk & ruthless for 5 actions
+  def silk_scraps()(implicit c: Character) {
     gear.shadowy()
-    if (c.qualities("Casing...") < 10)
-      flit.casing_optimal()
-    else
-      flit.steal_tales_of_terror()
+    c.travel(Areas.Spite)
+    c.beginStorylet("Unfinished Business in Spite")
+    c.chooseBranch("Encourage the honest folk of Spite to give you 'gifts'")
+    c.onwards()
+  }
+  
+  /**********/
+  /* RUMOUR */
+  /**********/
+  //+25 material 
+  def proscribed_material()(implicit c: Character) {
+    gear.dangerous()
+    c.travel(Areas.WatchmakersHill)
+    c.beginStorylet("Unfinished Business in Watchmaker's Hill")
+    c.chooseBranch("A literary sort of fist-fight")
+    c.onwards()
+  }
+  
+  /**************/
+  /* WILD WORDS */
+  /**************/
+  //+50 shrieks
+  def primordial_shrieks()(implicit c: Character) {
+    gear.watchful()
+    c.travel(Areas.LadybonesRoad)
+    c.beginStorylet("Unfinished Business in Ladybones Road")
+    c.chooseBranch("Debate the nature of Primordial Shrieks")
+    c.onwards()
+  }
+  
+  /*********/
+  /* WINES */
+  /*********/
+  //+520 '79 for 5 actions
+  def greyfields_1879()(implicit c: Character) {
+    gear.persuasive()
+    c.travel(Areas.Veilgarden)
+    c.beginStorylet("Unfinished Business in Veilgarden")
+    c.chooseBranch("Your crowd of admirers")
+    c.onwards()
+  }
+  
+  //+50 '82
+  def greyfields_1882()(implicit c: Character) {
+    gear.watchful()
+    c.travel(Areas.LadybonesRoad)
+    c.beginStorylet("Unfinished Business in Ladybones Road")
+    c.chooseBranch("A tradition developed after the Fall")
+    c.onwards()
   }
 }
