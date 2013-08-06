267876d108db11ca4e17589cbbb6a1ba5f9fae4c
Thomas Castiglione <castiglione@gmail.com>
2013-08-06 22:39:46 +0800

    filled out the grind

--- src/main/scala/david/convert.scala
+++ src/main/scala/david/convert.scala
@@ -1,33 +1,44 @@
 package david
 import london._
 
-object convert {
-  //Mysteries
-  def whispered_to_cryptic()(implicit c: Character) {
-    c.useItem("Whispered Secret")
-    c.chooseBranch("Combine 500 Whispered Secrets into 200 Cryptic Clues")
+object convert { 
+  /*************/
+  /* ACADEMIC */
+  /*************/
+  def candles_to_salts()(implicit c: Character) {
+    c.useItem("Foxfire Candle Stub")
+    c.chooseBranch("Make a substantial donation to F.F. Gebrandt's church")
+    c.onwards()    
+  }
+  
+  def salts_to_memories()(implicit c: Character) {
+    c.useItem("Abominable Salts")
+    c.chooseBranch("Sell your poison to zailors bound for the Elder Continent")
+    c.onwards()    
+  }
+  
+  /***************/
+  /* CARTOGRAPHY */
+  /***************/
+  def glim_to_scraps()(implicit c: Character) {
+    c.useItem("Glim")
+    c.chooseBranch("Trade a sack of glim")
     c.onwards()
   } 
   
-  def cryptic_to_appalling()(implicit c: Character) {
-    c.useItem("Cryptic Clue")
-    c.chooseBranch("Trade a great many Cryptic Clues")
-    c.onwards()
-  } 
-
-  def appalling_to_infamy()(implicit c: Character) {
-    c.useItem("Appalling Secret")
-    c.chooseBranch("Spend a night with the cats of the Duchess")
+  def scraps_to_ztories()(implicit c: Character) {
+    c.useItem("Map Scrap")
+    c.chooseBranch("Show your map scraps to a gathering of zee-captains")
     c.onwards()
   } 
   
-  def appalling_to_terror()(implicit c: Character) {
-    c.useItem("Appalling Secret")
-    c.chooseBranch("Correspond with a trio of sisters")
-    c.onwards()
-  } 
+  /***************/
+  /* ELDER */
+  /***************/
   
-  //Infernal
+  /************/
+  /* INFERNAL */
+  /************/
   def souls_to_sherry()(implicit c: Character) {
     c.useItem("Soul")
     c.chooseBranch("Offer a great many souls to the Embassy")
@@ -58,23 +69,135 @@ object convert {
     c.onwards()    
   }
   
-  //Influence
+  /*************/
+  /* INFLUENCE */
+  /*************/
+  def correspondence_to_gossip()(implicit c: Character) {
+    c.useItem("Stolen Correspondence")
+    c.chooseBranch("Spend a week decoding encrypted letters")
+    c.onwards()    
+  }
+
+  def gossip_to_documents()(implicit c: Character) {
+    c.useItem("Intriguing Gossip")
+    c.chooseBranch("Just write the things down")
+    c.onwards()    
+  }
   
-  //Academic
+  /**************/
+  /* LUMINOSITY */
+  /**************/
+  def beeswax_to_scarabs()(implicit c: Character) {
+    c.useItem("Lamplighter Beeswax")
+    c.chooseBranch("Offload a great quantity of wax")
+    c.onwards()    
+  }
   
-  //Luminosity
+  def scarabs_to_memories()(implicit c: Character) {
+    c.useItem("Phosphorescent Scarab")
+    ???
+    c.onwards()    
+  }
   
-  //Rumour
+  /*************/
+  /* MYSTERIES */
+  /*************/
+  def whispered_to_cryptic()(implicit c: Character) {
+    c.useItem("Whispered Secret")
+    c.chooseBranch("Combine 500 Whispered Secrets into 200 Cryptic Clues")
+    c.onwards()
+  } 
   
-  //Cartography
+  def cryptic_to_appalling()(implicit c: Character) {
+    c.useItem("Cryptic Clue")
+    c.chooseBranch("Trade a great many Cryptic Clues")
+    c.onwards()
+  } 
+
+  def appalling_to_infamy()(implicit c: Character) {
+    c.useItem("Appalling Secret")
+    c.chooseBranch("Spend a night with the cats of the Duchess")
+    c.onwards()
+  } 
   
-  //Elder
+  def appalling_to_terror()(implicit c: Character) {
+    c.useItem("Appalling Secret")
+    c.chooseBranch("Correspond with a trio of sisters")
+    c.onwards()
+  } 
   
-  //Wines
+  /*************/
+  /* NOSTALGIA */
+  /*************/
+  def honey_to_notions()(implicit c: Character) {
+    c.useItem("Drop of Prisoner's Honey")
+    c.chooseBranch("Sponsor a dream-expedition for lovers")
+    c.onwards()
+  } 
   
-  //Nostalgia
+  def notions_to_visions()(implicit c: Character) {
+    c.useItem("Romantic Notion")
+    c.chooseBranch("Hold a salon dedicated to nostalgia for the Surface")
+    c.onwards()
+  } 
+
+  /*************/
+  /* RAG TRADE */
+  /*************/
+  def silk_to_surface_silk()(implicit c: Character) {
+    c.useItem("Silk Scrap")
+    c.chooseBranch("Swap a great deal of silk")
+    c.onwards()
+  } 
   
-  //Rag Trade
+  def surface_silk_to_whisper_satin()(implicit c: Character) {
+    c.useItem("Surface-Silk Scrap")
+    c.chooseBranch("Take an armful of bolts to the Marred Mercer")
+    c.onwards()
+  } 
   
-  //Wild Words
+  /**********/
+  /* RUMOUR */
+  /**********/
+  def material_to_inklings()(implicit c: Character) {
+    c.useItem("Proscribed Material")
+    c.chooseBranch("Study an armful of forbidden books")
+    c.onwards()
+  } 
+  
+  def inklings_to_gossip()(implicit c: Character) {
+    c.useItem("Inkling of Identity")
+    c.chooseBranch("Agree to forget an identity you've confirmed")
+    c.onwards()
+  } 
+  
+  /**************/
+  /* WILD WORDS */
+  /**************/
+  def shrieks_to_prayers()(implicit c: Character) {
+    c.useItem("Primordial Shrieks")
+    c.chooseBranch("Let your shrieks loose on the Square of Lofty Words")
+    c.onwards()
+  } 
+  
+  def prayers_to_plaques()(implicit c: Character) {
+    c.useItem("Maniac's Prayer")
+    c.chooseBranch("Repeat a great many Maniac's Prayers")
+    c.onwards()
+  } 
+  
+  /*********/
+  /* WINES */
+  /*********/
+  def greyfields_to_morelways()(implicit c: Character) {
+    c.useItem("Greyfields 1882")
+    c.chooseBranch("Help out with the harvest festival")
+    c.onwards()
+  } 
+  
+  def morelways_to_absinthe()(implicit c: Character) {
+    c.useItem("Morelways 1872")
+    c.chooseBranch("Trade them off to the Portly Sommelier")
+    c.onwards()
+  } 
 }
--- src/main/scala/jobs/standardGrind.scala
+++ src/main/scala/jobs/standardGrind.scala
@@ -3,98 +3,69 @@ import london._
 import common._
 import david._
 
-//For efficient convertibility, 10/50/250/etc echoes are required at each tier 
-object standardGrind extends BufferedJob {
-  def apply(implicit c: Character): Boolean = {
-    avertMenaces.apply || playCards.apply || reduceMenaces || stockpileT1(10) || achieveGoals || stockpileT2(50) or money
+object standardGrind extends OneManJob {
+  def apply(implicit c: Character) = {
+    avertMenaces.apply || playCards.apply || stockpile || achieveGoals or money
   }
 
-  def reduceMenaces = false
+  //For efficient convertibility, 10/50/250/etc echoes are required at each tier
+  def stockpile(implicit c: Character) = farmT1(10) || farmT2(10)
   
   def achieveGoals(implicit c: Character) = flitGrind(c) 
   
-  def stockpileT1(echoes: Int)(implicit c: Character) = did (c.items("Whispered Secret") < (100 * echoes)) {
-    ???
+  def farmT1(echoes: Int)(implicit c: Character) = did (c.items("Cryptic Clue") < (50 * echoes)) {
+    grind.cryptic_clues()
   } or (c.items("Soul") < (50 * echoes)) {
-    ???
+    grind.souls()
   } or (c.items("Stolen Correspondence") < (20 * echoes)) {
-    ???
+    grind.stolen_correspondence()
   } or (c.items("Foxfire Candle Stub") < (100 * echoes)) {
-    ???
+    grind.foxfire_candle_stubs()
   } or (c.items("Lamplighter Beeswax") < (100 * echoes)) {
-    ???
+    grind.lamplighter_beeswax()
   } or (c.items("Proscribed Material") < (25 * echoes)) {
-    ???
+    grind.proscribed_material()
   } or (c.items("Glim") < (100 * echoes)) {
-    ???
+    grind.glim()
   } or (c.items("Jade Fragment") < (100 * echoes)) {
-    ???
+    grind.jade_fragments()
   } or (c.items("Greyfields 1882") < (50 * echoes)) {
-    ???
+    grind.greyfields_1882()
   } or (c.items("Drop of Prisoner's Honey") < (50 * echoes)) {
-    ???
+    grind.prisoners_honey()
   } or (c.items("Silk Scrap") < (100 * echoes)) {
-    ???
+    grind.silk_scraps()
   } or (c.items("Primordial Shriek") < (50 * echoes)) {
-    ???
+    grind.primordial_shrieks()
   }
   
-  def stockpileT2(echoes: Int)(implicit c: Character) = did (c.items("Appalling Secret") < ((8.0/3.0) * echoes)) {
-    if (c.items("Cryptic Clue") >= 500) {
-      convert.cryptic_to_appalling()
-    } else {
-      convert.whispered_to_cryptic()
-    }
+  def farmT2(echoes: Int)(implicit c: Character) = did (c.items("Appalling Secret") < ((8.0/3.0) * echoes)) {
+    convert.cryptic_to_appalling()
   } or (c.items("Amanita Sherry") < (10 * echoes)) {
     convert.souls_to_sherry()
   } or (c.items("Intriguing Gossip") < (5 * echoes)) {
-    ???
+    convert.correspondence_to_gossip()
   } or (c.items("Abominable Salts") < (10 * echoes)) {
-    ???
+    convert.candles_to_salts()
   } or (c.items("Phosphorescent Scarab") < (10 * echoes)) {
-    ???
+    convert.beeswax_to_scarabs()
   } or (c.items("Inkling of Identity") < (10 * echoes)) {
-    ???
+    convert.material_to_inklings()
   } or (c.items("Map Scrap") < (10 * echoes)) {
-    ???
+    convert.glim_to_scraps()
   } or (c.items("Relic of the Third City") < (10 * echoes)) {
-    ???
+    grind.relics_of_the_third_city()
   } or (c.items("Morelways 1872") < (10 * echoes)) {
-    ???
+    convert.greyfields_to_morelways()
   } or (c.items("Romantic Notion") < (10 * echoes)) {
-    ???
+    convert.honey_to_notions()
   } or (c.items("Surface-Silk Scrap") < (10 * echoes)) {
-    ???
+    convert.silk_to_surface_silk()
   } or (c.items("Maniac's Prayer") < (10 * echoes)) {
-    ???
+    convert.shrieks_to_prayers()
   }
   
   def money(implicit c: Character) {
     grind.tales_of_terror()	//for later use with the Fidgeting Writer
   }
-  
-  
-  
-/*
-        # keep 1000 of this around for the presumptuous little opportunity
-        if (character.persuasive < 85
-            and character.items['Greyfields 1882'] < 1000
-            and character.qualities['Scandal'] < 7):
-            grind.greyfields_1882(character)
-
-        # save up for lodgings
-        elif character.items['Whispered Secret'] < 80000:
-            grind.whispered_secrets(character)
-
-        elif character.items['Soul'] < 40000:
-            grind.souls(character)
-
-        # get money with risk of scandal
-        elif character.qualities['Scandal'] < 7:
-            grind.jade_watchful(character)
-
-        # get money with no risk
-        else:
-            grind.cryptic_clues(character)
- */
 }
