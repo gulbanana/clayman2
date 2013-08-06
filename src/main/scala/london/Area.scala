2277b43cc092fa832e527b28192411d132a0bf45
Thomas Castiglione <castiglione@gmail.com>
2013-08-06 09:45:40 +0800

    area

--- src/main/scala/london/Area.scala
+++ src/main/scala/london/Area.scala
@@ -18,6 +18,7 @@ object Areas {
   val University = Area(23, "The University")
   val Court = Area(26, "The Empress' Court")
   val LabyrinthOfTigers = Area(28, "The Labyrinth of Tigers")
+  val BazaarSideStreets = Area(31, "Bazaar Side-streets")
   val HouseOfChimes = Area(34, "The House of Chimes")
   val GamekeepersCottage = Area(5821, "The Gamekeeper's Cottage")
   
@@ -36,7 +37,8 @@ object Areas {
     18 -> Carnival,
     23 -> University,
     26 -> Court,
-    28 -> LabyrinthOfTigers,  
+    28 -> LabyrinthOfTigers,
+    31 -> BazaarSideStreets,
     34 -> HouseOfChimes,
     5821 -> GamekeepersCottage
   ) withDefault(i => { println("WARNING: unknown areaid %d".format(i)); Area(i, "Unknown Area")})
