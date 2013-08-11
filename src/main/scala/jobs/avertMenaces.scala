a9ca51e83d48128b2a230a45852d8363b50714c6
Thomas Castiglione <castiglione@gmail.com>
2013-08-11 13:21:44 +0800

    avert late to take opportunities to avoid it

--- src/main/scala/jobs/avertMenaces.scala
+++ src/main/scala/jobs/avertMenaces.scala
@@ -12,12 +12,12 @@ object avertMenaces extends RepeatedJob {
     prison.opportunities.played() or prison.reduce_suspicion()
   }
   
-  def reduceMenaces(implicit c: Character) = did (c.qualities("Scandal") > 4) { //this depends on luck, so start it ASAP
+  def reduceMenaces(implicit c: Character) = did (c.qualities("Scandal") > 5) { //this depends on luck, so start it ASAP
     c.travel(Areas.Lodgings)
     c.beginStorylet("Attend a Church Service")
     c.chooseBranch()
     c.onwards()
-  } or (c.qualities("Nightmares") > 5) {  //may increase scandal, so it comes after
+  } or (c.qualities("Nightmares") > 6) {  //may increase scandal, so it comes after
     c.travel(Areas.Carnival)
     if (c.items("Carnival Ticket") < 1) {
       c.beginStorylet("Buy tickets to the Carnival")
@@ -27,7 +27,7 @@ object avertMenaces extends RepeatedJob {
       c.chooseBranch("Try some hot wine")
     }
     c.onwards()
-  } or (c.qualities("Wounds") > 5) {  //reconsider this.. it might be faster just to die
+  } or (c.qualities("Wounds") > 6) {  //reconsider this.. it might be faster just to die
     c.travel(Areas.Lodgings)
     c.beginStorylet("Time in bed")
     val bestSleep = (c.branches - "A remarkable tincture").max(Ordering.by(guestPriorities))
@@ -36,7 +36,8 @@ object avertMenaces extends RepeatedJob {
   }
   
   private val guestPriorities = Map(
-    "Visions in the mirror" -> -1,  //nightmares on failure!
+    "Visions in the mirror" -> -2,  //nightmares on failure!
+    "Spend a day in bed" -> -1, //no healing on failure
     "A visit from the Wry Functionary" -> 1,
     "A visit from the Repentant Forger" -> 1,
     "A visit from the Sardonic Music-Hall Singer" -> 1,
