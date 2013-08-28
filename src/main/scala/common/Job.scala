e1f0e4a13b2e4abb358ca8cec542ba073300fd54
Thomas Castiglione <castiglione@gmail.com>
2013-08-28 19:10:11 +0800

    allow for buffer jobs to have nothing to do

--- src/main/scala/common/Job.scala
+++ src/main/scala/common/Job.scala
@@ -16,11 +16,10 @@ trait OneManJob extends Job with Duty {
 trait BufferedJob extends Job with Duty {
   def apply() = with_character { c =>
     val buffer = c.actionCap - 4
-    while (c.actions > buffer)
-      apply(c)
+    while (c.actions > buffer && apply(c)) {}
   }
 }
 
 trait RepeatedJob extends Job with Duty {
-  def apply() = with_character(c => while (c.actions > 0 && apply(c)) ())
+  def apply() = with_character(c => while (c.actions > 0 && apply(c)) {})
 }
