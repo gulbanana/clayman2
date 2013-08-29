8c305e934769692e807aa1cec04a2cb0b48d3dcd
Thomas Castiglione <castiglione@gmail.com>
2013-08-29 21:50:49 +0800

    zee cards

--- src/main/scala/david/unterzee/zailing.scala
+++ src/main/scala/david/unterzee/zailing.scala
@@ -21,6 +21,7 @@ object zailing {
     "The Clinging Coral Mass" -> Playable(implicit c => {gear.persuasive(); c.chooseBranch("'Put your backs into it, lads!'")}),            //+2 journey
     "The Fleet of Truth" -> Playable(implicit c => {gear.dangerous(); c.chooseBranch("Villainy!")}),                                        //+journey, +5 of each type of notes
     "A Corvette of Her Majesty's Navy" -> Conditional(_.qualities("Suspicion") < 5, _.chooseBranch("Exchange pleasantries via semaphore")), //+1 journey, -1 troubled
+    "A Wily Zailor" -> Trivial,                                                                                                             //XXX gets better with exzperience, so change this
     "Calm Seas: Fair Zailing" -> Trivial,                                                                                                   //+4 journey, +3 troubled, +SIC
     "Calm Seas: A Spit of Land" -> Playable(_.chooseBranch("Steam on by")),                                                                 //+2 journey, +2 troubled
     "Calm Seas: A Steamer full of Passengers" -> Playable(_.chooseBranch("Invite them aboard for a party")),                                //+hedonist, 1 stockings, 5 secluded addresses - 1.40E (but +troubled)
