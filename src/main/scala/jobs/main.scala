c6b3c6e5d4bd86d00f19c8c73c0ba496cf5fdea9
Thomas Castiglione <castiglione@gmail.com>
2013-09-01 13:25:28 +0800

    ~functions~

--- src/main/scala/jobs/main.scala
+++ src/main/scala/jobs/main.scala
@@ -5,13 +5,13 @@ import david._
 
 object main extends BufferedJob {
   def apply(implicit c: Character) = c.location match {
-    case Areas.NewNewgate             => prison.opportunities.played() or prison.reduce_suspicion()
-    case Areas.TombColonies           => colonies.opportunities.played() or colonies.reduce_scandal()
-    case Areas.BroadUnterzee          => unterzee.southernArchipelago(c)
-    case Areas.SeaOfVoices            => unterzee.seaOfVoices(c)
-    case Areas.GruntingFen            => unterzee.gruntingFen(c)
-    case x if londonAreas.contains(x) => london.standardGrind(c)
-    case _                            => false
+    case Areas.NewNewgate    => prison.opportunities.played() or prison.reduce_suspicion()
+    case Areas.TombColonies  => colonies.opportunities.played() or colonies.reduce_scandal()
+    case Areas.BroadUnterzee => unterzee.southernArchipelago(c)
+    case Areas.SeaOfVoices   => unterzee.seaOfVoices(c)
+    case Areas.GruntingFen   => unterzee.gruntingFen(c)
+    case x if londonAreas(x) => london.standardGrind(c)
+    case _                   => false
   } 
   
   private val londonAreas = Set(
