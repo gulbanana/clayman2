0daa10086beffb385270e89f2fbb1eef6428be53
Thomas Castiglione <castiglione@gmail.com>
2013-09-19 12:52:39 +0800

    automated Bullbone Island

--- src/main/scala/david/unterzee/islands.scala
+++ src/main/scala/david/unterzee/islands.scala
@@ -45,7 +45,46 @@ object islands {
     c.onwards()
   }
   
-  def bullbone_T1()(implicit c: Character) {
-    
+  def bullbone_T0()(implicit c: Character) {
+    c.beginStorylet("Bullbone Island")
+    c.chooseBranch("The bones of Bullbone")
+    c.onwards()
+  }
+  
+  def bullbone_T4()(implicit c: Character) {
+    c.beginStorylet("The Little Cave")
+    c.chooseBranch("Oho - a little writing")
+    c.onwards()
+  }
+  
+  def bullbone_T6()(implicit c: Character) {
+    c.beginStorylet("Where the Wild Mandrakes Grow")
+    c.chooseBranch("Creaking in the breeze")
+    c.onwards()
+  }
+  
+  def bullbone_T7()(implicit c: Character) {
+    c.beginStorylet("Sparkling around the Copse")
+    c.chooseBranch("Acquisition and screaming") //gain glim, the other option is moon-pearls
+    c.onwards()
+  }
+  
+  def bullbone_T8()(implicit c: Character) {
+    c.beginStorylet("Spiritual happenings")
+    c.chooseBranch("Search the island")         //theosophistical notes (alt: aeolian screams)
+    c.onwards()
+  }
+  
+  def bullbone_T9()(implicit c: Character) {
+    c.beginStorylet("Finish your notes on Bullbone Island")
+    c.chooseBranch("Write up your notes")
+    c.onwards()
+  }
+  
+  def bullbone_T10()(implicit c: Character) {
+    c.beginStorylet("Orthos has Found You")
+    c.chooseBranch("Tarry a little")
+    c.onwards()
+    c.onwards()
   }
 }
--- src/main/scala/jobs/unterzee/bullboneIsland.scala
+++ src/main/scala/jobs/unterzee/bullboneIsland.scala
@@ -1,12 +1,26 @@
 package jobs.unterzee
 import api._
 import common._
+import david.gear
 import david.unterzee._
 
-object bullboneIsland extends RepeatedJob {
-  def apply(implicit c: Character) = did (c.qualities("Orthos is Coming!") < 0) {
-    islands.bullbone_T1()
-  } or (c.qualities("Orthos is Coming!") > 100) {
-    islands.bullbone_T1()
+object bullboneIsland extends OneManJob {
+  def apply(implicit c: Character) = {
+    gear.watchful()
+    did (c.qualities("Orthos is Coming!") < 4) {
+      islands.bullbone_T0()
+    } or (c.qualities("Orthos is Coming!") < 6) {
+      islands.bullbone_T4()
+    } or (c.qualities("Orthos is Coming!") == 6) {
+      islands.bullbone_T6()
+    } or (c.qualities("Orthos is Coming!") == 7) {
+      islands.bullbone_T7()
+    } or (c.qualities("Orthos is Coming!") == 8) {
+      islands.bullbone_T8()
+    } or (c.qualities("Orthos is Coming!") == 9) {
+      islands.bullbone_T9()
+    } or (c.qualities("Orthos is Coming!") == 10) {
+      islands.bullbone_T10()
+    }
   }
 }
