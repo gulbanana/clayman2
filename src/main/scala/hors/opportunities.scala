921ae5a45ab967ff8f338aebe49bf64fc82d384b
Thomas Castiglione <castiglione@gmail.com>
2013-10-05 19:42:55 +0100

    start classifying socialbot cards

--- src/main/scala/hors/opportunities.scala
+++ src/main/scala/hors/opportunities.scala
@@ -1,6 +1,23 @@
 package hors
 import common._
 
-object opportunities extends Opportunist(Map(
-  "Pass the Cat: a wriggling delivery" -> Hold
-), default = Discard)
+object cards {
+  val standard = Map(
+    "Academic discipline" -> Discard,   //dangerous 45
+    "Moonish water" -> Discard,         //dangerous 57
+    "After dinner speaking" -> Discard, //persuasive 33
+    "The Windward Tower" -> Hold        //lodgings
+  )
+  
+  val story = Map(
+    "The independence of young ladies" -> Hold //clathermont 1
+  )
+    
+  val social = Map(
+    "Pass the Cat: a wriggling delivery" -> Hold,
+    "Give a Gift! A commotion in the Square of Lofty Words" -> Hold
+  )
+}
+
+object opportunities extends Opportunist(cards.standard ++ cards.story ++ cards.social) {
+}
